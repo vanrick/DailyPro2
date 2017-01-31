6e2c6cb8fd332dc26eb7a1ffcdec918a4b4ef43d
Rick Van <x.rckvan@gmail.com>
2017-01-31 15:53:55 -0700

    fix count letters solution

--- 01_countLetters/rv_solution.js
+++ 01_countLetters/rv_solution.js
@@ -1,18 +1,25 @@
 var string = 'happy days'
-var fox = "The quick brown fox jumps over the lazy dog and the sleeping cat early in the day"
+var fox = "The quick brown 1 fox jumps over the lazy dog and the sleeping cat early in the day"
 var int = 12345
-function cnt(string){
-  string=string.toLowerCase().replace(/\s/g, '').split('')
-  var bucket = {}
-    for (var i = 0; i < string.length; i++) {
-      if (bucket[string[i]]) {
-        bucket[string[i]]++
-      }else{
-        bucket[string[i]]=1
-      }
-    }
+var mix = "hey " + 1 + " no"
 
-console.log(bucket);
-return bucket
+function cnt(string){
+  if (typeof string == 'string') {
+    string=string.toLowerCase().replace(/[\s\d]/g,"").split('')
+    var bucket = {}
+      for (var i = 0; i < string.length; i++) {
+        if (bucket[string[i]]) {
+          bucket[string[i]]++
+        }else{
+          bucket[string[i]]=1
+        }
+      }
+      console.log(bucket);
+      return bucket
+  }else{
+    console.log('no its not!');
+    return 'no its not!'
+  }
+
 }
-cnt(fox)
+cnt(int)
---
+++ 02_rampNumbers/rv_solution.js
@@ -0,0 +1,8 @@
+var num = 1234;
+var num2 = 1032;
+
+function ramp(number){
+  for (var i = 0; i < number.length; i++) {
+    number[i]
+  }
+}
