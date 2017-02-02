bd9ab6b946eaffc69eb910037487edc0d899c789
Rick Van <x.rckvan@gmail.com>
2017-02-01 18:29:32 -0700

    ramped numbers

--- 02_rampNumbers/rampNumbers.js
+++ 02_rampNumbers/rampNumbers.js
@@ -5,17 +5,20 @@ var numbers = 1234
 var nonNumbers = 1034
 var test = 1123
 var test2 = 1143
+var num3 = 1113
 
 function isRamped(num){
   var arr = num.toString().split('')
   for (var i = 0; i < arr.length-1; i++) {
+    console.log(arr.length-1);
     if (arr[i] <= arr[i+1]) {
+      console.log('1st ',arr[i]);
+      console.log('2nd ',arr[i+1]);
       console.log("hi");
     }else{
       return false
     }
   }
-
   return true
 }
-console.log(isRamped(test2));
+console.log(isRamped(num3));
--- 02_rampNumbers/rv_solution.js
+++ 02_rampNumbers/rv_solution.js
@@ -1,8 +1,24 @@
 var num = 1234;
 var num2 = 1032;
+var num3 = 1113;
+var num4 = 1143;
+var not = 'hello!';
 
 function ramp(number){
-  for (var i = 0; i < number.length; i++) {
-    number[i]
-  }
+  number = number.toString().split('')
+  if (Number.isInteger(parseInt(number)) === true) {
+    for (var i = 0; i < number.length-1; i++) {
+      if (number[i] <= number[i+1]) {
+          console.log('1st ',number[i]);
+          console.log('2nd ',number[i+1]);
+          console.log('Cool!');
+       }else{
+         return false
+       }
+      }
+      return true
+    }
+  console.log('Please enter numbers');
 }
+
+console.log(ramp(num4))
