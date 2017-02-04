f22e785f54bbd1dd0e8c6201a0b4e0017f5d9077
Rick Van <x.rckvan@gmail.com>
2017-02-03 20:03:53 -0700

    anagrams!

---
+++ 04_anagrams/rv_solutions.js
@@ -0,0 +1,22 @@
+var word = 'examples1'
+var ana = 'pameesxl1'
+var anaLong = 'pameesxll'
+var notAna = 'fagegf'
+var num = 11234
+
+function anie(word,ana){
+  if (Number.isInteger(word) != true && Number.isInteger(ana) != true) {
+    word = word.split('').sort().join('')
+    ana = ana.split('').sort().join('')
+    if (word.length !== ana.length || word !== ana) {
+      return false
+    }else{
+      return true
+    }
+  }else{
+    console.log('enter only strings');
+    return false
+  }
+}
+
+console.log(anie(word,ana));
