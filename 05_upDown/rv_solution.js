36c8b3b2f3b000057ab113d385321e9faab6a147
Rick Van <x.rckvan@gmail.com>
2017-02-06 19:37:55 -0700

    fixed upDown

--- 05_upDown/rv_solution.js
+++ 05_upDown/rv_solution.js
@@ -1,18 +1,23 @@
 var num = [6,3,5,4,3,4,4,5]
+var str = 'Cheese Burgers!'
 
 function upDown (num){
-  var bucket = []
-  for (var i = 0; i < num.length; i++) {
-    console.log(num[i]);
-    if (num[i] < num[i+1]) {
-      bucket.push('up')
-    }else if(num[i] > num[i+1]){
-      bucket.push('down')
-    }else if(num[i] == num[i+1]){
-      bucket.push('even')
+  if (Number.isInteger(Number(num.toString().split(',').join('')))) {
+    var bucket = []
+    for (var i = 0; i < num.length; i++) {
+      console.log(num[i]);
+      if (num[i] < num[i+1]) {
+        bucket.push('up')
+      }else if(num[i] > num[i+1]){
+        bucket.push('down')
+      }else if(num[i] == num[i+1]){
+        bucket.push('even')
+      }
     }
+  }else{
+    console.log('Please enter ONLY integers');
   }
   return bucket
 }
 
-console.log(upDown(num));
+console.log(upDown(str));
