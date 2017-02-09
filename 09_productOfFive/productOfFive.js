1120568580c1507378991e8d685b312b05a48646
Rick Van <x.rckvan@gmail.com>
2017-02-08 21:28:28 -0700

    product of five

--- 09_productOfFive/productOfFive.js
+++ 09_productOfFive/productOfFive.js
@@ -2,16 +2,20 @@
 //09 Product of Five
 
 var numbers = '127289373913828309127381263'
+var large = '111333321'
+var num = '00000'
 
 function productOfFive(input){
 
-  var sorted = numbers.split('').sort(function (a, b) {  return b - a;  }).join('')
+  var sorted = input.split('').sort(function (a, b) {  return b - a;  }).join('')
+  console.log(sorted);
   // var convNums = parseInt(sorted)
   // input.sort(function (a, b) {  return a - b;  });
   var arr = []
   for (var i = 0; i < 5; i++) {
     arr.push(parseInt(sorted[i]))
   }
+  console.log(arr);
   return arr.reduce(function(a,b){
     return a * b
   })
---
+++ 09_productOfFive/rv_solution.js
@@ -0,0 +1,18 @@
+let largest = 127289373913828309127381263;
+let largest2 = 111333321
+
+function findProduct (input){
+  let bucket = []
+  input = input.toString().split('').sort().reverse()
+  // let input2 = input.sort().reverse()
+  // console.log(input);
+  for (var i = 0; i < 5; i++) {
+
+    bucket.push(input[i])
+  }
+  return bucket.reduce(function(a,b){
+    return a * b
+  })
+}
+
+console.log(findProduct(largest2))
