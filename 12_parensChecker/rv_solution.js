22eda36eb8b2f36627d3861ea14c9811a306a518
Rick Van <x.rckvan@gmail.com>
2017-02-11 20:05:56 -0700

    parens

---
+++ 12_parensChecker/rv_solution.js
@@ -0,0 +1,21 @@
+let parens = "()(((())))"
+let nonParens = "())))("
+let nonParens2 = ')('
+
+function parensChecker(string){
+  string = string.split('').join('')
+  let bucket = 0
+  for (var i = 0; i < string.length; i++) {
+    if (string[i]==='(') {
+      bucket++
+    }else if(string[i]===')'){
+      bucket--
+    }
+    if(bucket < 0){
+      return false
+    }
+  }
+  return bucket === 0
+}
+
+console.log(parensChecker(nonParens2))
