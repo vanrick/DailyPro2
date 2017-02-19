ddcf1c4e46befb4bc2ec19f797bc1faa97dbeb90
Rick Van <x.rckvan@gmail.com>
2017-02-18 21:19:43 -0700

    revised with every step

--- 18_gameOfThrees/solutions/rv_solution.js
+++ 18_gameOfThrees/solutions/rv_solution.js
@@ -3,19 +3,20 @@ let num1 = 11
 
 
 function gameOfThrees(num){
-  let bucket = 0
-  if (num % 3 == 0) {
-    console.log('bucket 3', bucket);
-    bucket = num + " " + 0
-  }else if(num % 3 == 2){
-    console.log('bucket 2', bucket);
-    bucket = num + " " + 1
-  }else if(num % 3 == 1){
-    console.log('bucket 4', bucket);
-    bucket = num + ' ' + '-1'
+  while (num > 1) {
+    if (num % 3 == 0) {
+      console.log('num 0', num, " ", 0);
+      num = num/3
+    }else if((num + 1) % 3 == 0){
+      console.log('num +1', num, ' ', 1);
+      num = (num + 1)/3
+    }else if((num -1) % 3 == 0){
+      console.log('num -1', num, ' ', -1);
+      num = (num - 1)/3
+    }
   }
-  console.log(bucket);
-  return bucket
+  console.log(num);
+  return num
 }
 
-gameOfThrees(100)
+gameOfThrees(31337357)
