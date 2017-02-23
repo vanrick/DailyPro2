6d9aeaac9ff630bce7d0cd8e2fac7b1eb276acb7
Rick Van <x.rckvan@gmail.com>
2017-02-23 16:01:25 -0700

    working on json obj

--- 22_jsonFormatting/rv_solution.js
+++ 22_jsonFormatting/rv_solution.js
@@ -23,13 +23,28 @@ let obj =
 }
 
 function objToJson(input){
-  console.log(input.books);
+  // console.log(input.books);
+  let keys = Object.keys(input)
+  // console.log(keys);
   let json = {}
-  json.author = [{}]
-  console.log(json.author);
-  // for (var i = 0; i < input.length; i++) {
-  //   json.input[i]
-  // }
+  for (var i = 0; i < keys.length; i++) {
+    // console.log(input[keys[i]])
+    let identifiers = input[keys[i]]
+    for (var j = 0; j < identifiers.length; j++) {
+      // console.log(Object.keys(identifiers[j]))
+      let objKeys = identifiers[j]['id']
+      // console.log(identifiers[j]['id']);
+      if (objKeys == '1') {
+        console.log('1 ',identifiers[j]);
+        json[identifiers[j]]
+      }else if(objKeys == '2'){
+        // console.log('2 ',identifiers[j]);
+      }else{
+        
+      }
+    }
+  }
+
   console.log(json);
   return json
 }
