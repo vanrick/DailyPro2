5f4908cd2f7c0eea3604f20daa4cb6d72143bec0
Rick Van <x.rckvan@gmail.com>
2017-02-28 20:35:49 -0700

    math word problem

---
+++ 21_swissRecordDesign/rv_solution.js
@@ -0,0 +1,32 @@
+let mathQuestion = "What is 5 minus 3 and 8 plus 2 divided by 3"
+
+function mathy(input){
+  var second = input.replace(/\bwhat|is|b\S+/ig,"")
+  var first  = second.replace(/\s+/g,' ').trim().split(' ')
+  // console.log(first);
+  let bucket = 0
+  for (var i = 0; i < first.length; i++) {
+    if (first[i]=='minus') {
+      let a = first[i-1]
+      let b = first[i+1]
+      bucket = (Number(a) - Number(b))
+      // console.log(a,b);
+    }else if (first[i] == 'and'){
+      let a = bucket
+      let b = first[i+1]
+      bucket = (Number(a) + Number(b))
+    }else if(first[i] == 'plus'){
+      let a = bucket
+      let b = first[i+1]
+      bucket = (Number(a) + Number(b))
+    }else if(first[i]=='divided'){
+      let a = bucket
+      let b = first[i+1]
+      bucket = (Number(a)/Number(b))
+    }
+  }
+  console.log(bucket);
+  return bucket
+}
+
+mathy(mathQuestion)
