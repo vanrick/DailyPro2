f1b11d02b7ebf63557662a3e29a9d6f8452ceacf
Rick Van <x.rckvan@gmail.com>
2017-03-14 19:58:01 -0600

    quad problem

--- 34_arrays/rv_solution.js
+++ 34_arrays/rv_solution.js
@@ -1,1 +1,54 @@
-let arr = []
+var arr = [0, 3, 1, 4, 5, 80, 27, 58] //indexOf(6) //-1
+
+function sortNumber(a,b) {
+    return a - b;
+}
+
+function pramp(arr){
+  numArray = arr.sort(sortNumber)
+  let bucket = []
+  let max = 0
+  let min = 0
+  for (var i = 0; i < numArray.length; i++) {
+    if (numArray[i]> max) {
+      max = numArray[i]
+      // console.log(max);
+    }else if(numArray[i]<min){
+      min = numArray[i]
+      // console.log(min);
+    }
+  }
+  for (var i = min; i <= max; i++) {
+    bucket.push(i)
+  }
+  // console.log(bucket);
+  return bucket
+}
+
+pramp(arr)
+
+//quad problem
+
+var arr = [4,3,1,-5,7,8]
+var arr2 = [1,3,4,0,9,10]
+var s = 8
+
+function findSumOf(array, sum){
+
+let bucket = []
+  for (i in array){
+    for (var j = i+1; j < array.length; j++) {
+      for (var k = j+1; k < array.length; k++) {
+        for (var l = k+1; l < array.length; l++) {
+          if (array[i]+array[j]+array[k]+array[l] == sum) {
+            bucket.push(array[i],array[j],array[k],array[l])
+          }
+        }
+      }
+    }
+    console.log(bucket);
+    return bucket
+  }
+}
+
+findSumOf(arr2,s)
