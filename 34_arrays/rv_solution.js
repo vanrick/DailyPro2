37b6d7da415ae4b39cfd597645410f639108f66a
Rick Van <x.rckvan@gmail.com>
2017-03-17 20:01:26 -0600

    fixed the index problem

--- 34_arrays/rv_solution.js
+++ 34_arrays/rv_solution.js
@@ -1,4 +1,4 @@
-var arr = [0, 3, 1, 4, 5, 80, 27, 58] //indexOf(6) //-1
+// var arr = [0, 3, 1, 4, 5, 80, 27, 58] //indexOf(6) //-1
 
 function sortNumber(a,b) {
     return a - b;
@@ -25,31 +25,73 @@ function pramp(arr){
   return bucket
 }
 
-pramp(arr)
+// pramp(arr)
 
 //quad problem
 
-var arr = [4,3,1,-5,7,8]
-var arr2 = [2,9,2,0,8,-4]
-var s = 8
+// var arr = [4,3,1,-5,7,8]
+// var arr2 = [2,9,2,0,8,-4]
+// var s = 8
+//
+// function findSumOf(array, sum){
+//
+// let bucket = []
+//   for (i in array){
+//     for (var j = i+1; j < array.length; j++) {
+//       for (var k = j+1; k < array.length; k++) {
+//         for (var l = k+1; l < array.length; l++) {
+//           if (array[i]+array[j]+array[k]+array[l] == sum) {
+//              bucket.push(array[i],array[j],array[k],array[l])
+//           }
+//         }
+//       }
+//     }
+//   }
+//   console.log(bucket);
+//   return bucket
+// }
+//
+// findSumOf(arr2,s)
+
+//second version of quad problem
+
+const arr = [4, 0, -5, 1, 9, 8]
+const arr2 = [2, 2, 2, 2, 8, 10]
+const s = 8
 
 function findSumOf(array, sum){
 
-let bucket = []
-  for (i in array){
-    for (var j = i+1; j < array.length; j++) {
+  for (var i in array){
+    for (var j = Number(i)+1; j < array.length; j++) {
       for (var k = j+1; k < array.length; k++) {
         for (var l = k+1; l < array.length; l++) {
           if (array[i]+array[j]+array[k]+array[l] == sum) {
-             bucket.push(array[i],array[j],array[k],array[l])
-            // console.log([array[i],array[j],array[k],array[l]]);
+            return [array[i],array[j],array[k],array[l]]
           }
         }
       }
     }
   }
-  console.log(bucket);
-  return bucket
 }
 
-findSumOf(arr2,s)
+function findSumOfOther(array, sum){
+
+  for (var i = 0; i < array.length; i++){
+    for (var j = i+1; j < array.length; j++) {
+      for (var k = j+1; k < array.length; k++) {
+        for (var l = k+1; l < array.length; l++) {
+          if (array[i]+array[j]+array[k]+array[l] == sum) {
+            return [array[i],array[j],array[k],array[l]]
+          }
+        }
+      }
+    }
+  }
+}
+console.log("With for in loop")
+console.log(findSumOf(arr,s))
+console.log(findSumOf(arr2,s))
+
+console.log("\nWithout for in loop")
+console.log(findSumOfOther(arr, s))
+console.log(findSumOfOther(arr2, s))
