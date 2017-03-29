859b4aaec5dd3f6ef3ff5d402ada7720cdf0b463
Rick Van <x.rckvan@gmail.com>
2017-03-28 23:51:17 -0600

    wordy problem

---
+++ 35_wordy/rv_solution.js
@@ -0,0 +1,14 @@
+let word = 'What is 5 plus 13?'
+let word1 = 'What is 7 minus 5?'
+let word2 = 'What is 6 multiplied by 4?'
+let word3 = 'What is 25 divided by 5?'
+let word4 = 'What is 4 times 3 divided by 6?'
+
+
+function wordy(word){
+  word = word.split(' ')
+  
+  console.log(word);
+}
+
+wordy(word)
