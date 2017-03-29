0feb7ce12f60d85de8076381db47688356062644
Rick Van <x.rckvan@gmail.com>
2017-03-29 13:11:49 -0600

    wordy in progress

--- 35_wordy/rv_solution.js
+++ 35_wordy/rv_solution.js
@@ -6,9 +6,49 @@ let word4 = 'What is 4 times 3 divided by 6?'
 
 
 function wordy(word){
-  word = word.split(' ')
-  
-  console.log(word);
-}
+  var array = word.toLowerCase().replace(/\bwhat|\bis|\bby\b|\?/g, '').split(' ');
+  var newWord = array.toString().replace(/[, ]+/g, " ").trim()
+  var newWord2 = newWord.split(' ')
+  var newWord3 = newWord.split(' ')
+  // console.log(newWord2);
+  var result = []
 
-wordy(word)
+  for (var i = 0; i < newWord2.length; i++) {
+    // parseInt(newWord2[i])
+    // console.log(parseInt('parse ',newWord2[i]));
+    if (Number.isInteger(parseInt(newWord2[i])) === true) {
+      console.log(Number(newWord2[i]));
+      result.push(Number(newWord2[i]))
+    }
+  }
+    // for (var i = 0; i < newWord3.length; i++) {
+    //   parseInt(newWord3[i])
+    //   if (newWord3[i] =='multiplied' || newWord3[i]=='times') {
+    //     let a = result.pop()
+    //     let b = result.pop()
+    //     result.push(Number(a)*Number(b))
+    //     console.log('a ',a);
+    //     console.log('b ',b);
+    //   }else if(newWord3[i]=='plus'){
+    //     let a = result.pop()
+    //     let b = result.pop()
+    //     rresult.push(a+b)
+    //   }else if(newWord3[i]=='minus'){
+    //     let a = result.pop()
+    //     let b = result.pop()
+    //     result.push(a-b)
+    //   }
+    //   else if(newWord3[i]=='divided'){
+    //     let a = result.pop()
+    //     let b = result.pop()
+    //     result.push(Number(a)/Number(b))
+    //   }
+    // }
+let newResult = result.pop()
+  console.log(result);
+  return result
+}
+var fruits = ['Banana', 'Orange', 'Lemon', 'Apple', 'Mango'];
+var citrus = fruits.slice(1, 1);
+console.log(citrus);
+wordy(word4)
