c29297e35d21b4c747d107fa4efb1b37d77d45b6
Rick Van <x.rckvan@gmail.com>
2017-05-05 17:13:16 -0600

    higher

--- practice/main.js
+++ practice/main.js
@@ -1,34 +1,35 @@
-// let num = [3,5,2,4,9]
-//
-// function double(num){
-//   return num.map(function(number, anotherone){
-//     console.log(anotherone);
-//     return number * 2
-//   })
-// }
-//
-// console.log(double(num));
+let num = [3, 5, 2, 4, 9]
 
-// let numz = [3,5,2,4,9];
-//
-// function doublez(num){
-//   return num.reduce(function(prev,curr,index,array){
-//     prev[index] = curr
-//     return prev
-//   },{})
-// }
-//
-// console.log(doublez(numz));
-
-let nums = [3,5,2,4,9];
-
-function doubles(num){
-  let array = []
-  num.forEach((key)=>{
-    array.push(key * 2)
-  })
-  console.log(array);
-  return array
+function double(num) {
+    return num.map(function(number, index) {
+        console.log('Index: ', index);
+        return number * 2
+    })
 }
 
-doubles(nums)
+console.log(double(num));
+
+// let numz = [3, 5, 2, 4, 9];
+
+// function doublez(num) {
+//     return num.reduce(function(prev, curr, index, array) {
+//         prev[index] = curr
+//         return prev
+//     }, {})
+// }
+
+// console.log(doublez(numz));
+
+// let nums = [3, 5, 2, 4, 9];
+
+// function doubles(num) {
+//     let array = []
+//     num.forEach((key) => {
+//         array.push(key * 2)
+//     })
+//     console.log(array);
+//     return array
+// }
+
+
+// doubles(nums)
