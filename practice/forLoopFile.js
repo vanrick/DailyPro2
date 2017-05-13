ef1a2bbf530043afe2bec846b8ae4aaa54ab52a6
Rick Van <x.rckvan@gmail.com>
2017-05-12 19:16:05 -0600

    3d arrays

---
+++ practice/forLoopFile.js
@@ -0,0 +1,19 @@
+const threeDArray = [
+  [[[1,8],2,3],[4,5,6]],
+  [[7,8,9],[10,11,12]]
+]
+
+// function flatten(threeDArray){
+//   console.log(threeDArray.concat([[[13,14,15]]]))
+// }
+
+// let array2 = [].concat.apply([],threeDArray)
+
+console.log([].concat.apply([],[].concat.apply([],[].concat.apply([],threeDArray))));
+
+function flat(threeDArray){
+  
+  for (var i = 0; i < 3; i++) {
+    console.log([].concat.apply([],));
+  }
+}
