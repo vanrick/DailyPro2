9ea963febc6694fbcc47d8e2acae1036d35a8910
Rick Van <x.rckvan@gmail.com>
2017-05-29 22:25:44 -0600

    json conversion

---
+++ 19_wordCloud/rv_solutions2.js
@@ -0,0 +1,19 @@
+let words = {
+  "Ila Huels": [
+    "OPTIMIZE WEB-ENABLED SUPPLY-CHAINS",
+    "brand sexy channels",
+    "ENVISIONEER ROBUST E-COMMERCE",
+    "TRANSFORM WIRELESS ARCHITECTURES",
+    "BENCHMARK CROSS-PLATFORM PARTNERSHIPS"
+  ],
+  "Cristopher Feest": [
+    "BENCHMARK CROSS-PLATFORM PARTNERSHIPS",
+    "brand sexy channels",
+    "BENCHMARK 24/7 PARADIGMS"
+  ]
+}
+
+function convertObj(words){
+
+}
+convertObj(words)
