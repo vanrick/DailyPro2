4f5552506fcd651f190dc5913f9297cecac12a49
Rick Van <x.rckvan@gmail.com>
2017-05-30 18:12:48 -0600

    word cloud

--- 19_wordCloud/rv_solutions2.js
+++ 19_wordCloud/rv_solutions2.js
@@ -14,6 +14,28 @@ let words = {
 }
 
 function convertObj(words){
-
+  let keys = Object.keys(words)
+  let result = {}
+  for (var i = 0; i < keys.length; i++) {
+    let arrayOfWords = words[keys[i]]
+    for (var j = 0; j < arrayOfWords.length; j++) {
+      let indWords = arrayOfWords[j].split(' ')
+      for (var k = 0; k < indWords.length; k++) {
+        if (result[indWords[k]]) {
+          result[indWords[k]].count++
+          if (result[indWords[k]].people.indexOf(keys[i]) < 0) {
+            result[indWords[k]].people.push(keys[i])
+          }
+        }else{
+          result[indWords[k]] = {
+            count: 1,
+            people: [keys[i]]
+          }
+        }
+      }
+    }
+  }
+  console.log(result);
+  return result
 }
 convertObj(words)
---
+++ 20_sumDigits/rv_solutions2.js
@@ -0,0 +1,30 @@
+// function sumToSingle(num){
+//   do {
+//     var stringNum = String(num);
+//     let numToArr = stringNum.split('')
+//     var num = 0
+//     numToArr.forEach(function(key){
+//       num += Number(key)
+//     })
+//   } while (stringNum.length > 1);
+//
+//   console.log(num);
+//   return num
+// }
+//
+// sumToSingle(12345)
+
+function recurSums(num) {
+  let result = 0
+  num = String(num).split('');
+  num.forEach(function(ele){
+    result += Number(ele)
+  })
+  if (String(result).length > 1) {
+    recurSums(result)
+  }else{
+    console.log(result);
+    return result
+  }
+}
+recurSums(12345)
