f7c9a4bff439de5b71aff4627bdae722ef8feba7
Rick Van <x.rckvan@gmail.com>
2017-06-19 16:39:54 -0600

    practicing

--- practice/assess.js
+++ practice/assess.js
@@ -1,37 +1,23 @@
-// var y;
-//   function modifyVariables(x)
-//   {
-//     var z = 5;
-//     x += 2;
-//     y += x + z;
-//   }
-//
-//   var x = 1;
-//   var y = 2;
-//   var z = 3;
-//   modifyVariables(x);
-// console.log(x);
-//   console.log(y);
-//   console.log(z);
-
-let array = [1,2,3,4,5,6]
-
-function splitArray(array, num){
-  array = shuffleArray(array)
-  let divisor = array.length/num
-  let returnArray = []
+function splitArray(arr, num){
+  arr = shuffleArray(arr);
+  console.log(arr);
+  let divisor = arr.length/num
+  let = result = []
   for (var i = 0; i < num; i++) {
-      returnArray.push(array.slice(i*divisor, i*divisor + divisor))
+    result.push(arr.slice(i*divisor, i*divisor + divisor))
   }
-  return returnArray
+  console.log(result);
+  return result
 }
 
-console.log(splitArray(array,2))
+let array = [1,2,3,4,5,6]
+// slice(0,2) (2,4) (4,6)
+splitArray(array,3)
 
 function shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
         var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i]; //6
+        var temp = array[i]; // 6
         array[i] = array[j];  //random number
         array[j] = temp; // 6
     }
