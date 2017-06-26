ac4b1203f462a93e8a5ebb86c56533b1cb04a366
Rick Van <x.rckvan@gmail.com>
2017-06-26 12:40:42 -0600

    numbers

---
+++ 36_say/rv_solutions2.js
@@ -0,0 +1,47 @@
+let singles = {
+  '0': 'zero',
+  '1': 'one',
+  '2': 'two',
+  '3': 'three',
+  '4': 'four',
+  '5': 'five',
+  '6': 'six',
+  '7': 'seven',
+  '8': 'eight',
+  '9': 'nine',
+}
+let teens = {
+  '11': 'eleven',
+  '12': 'twelve',
+  '13': 'thirteen',
+  '14': 'fourteen',
+  '15': 'fifteen',
+  '16': 'sixteen',
+  '17': 'seventeen',
+  '18': 'eightteen',
+  '19': 'nineteen'
+}
+let tens ={
+  '10': 'ten',
+  '20': 'twenty',
+  '30': 'thirty',
+  '40': 'forty',
+  '50': 'fifty',
+  '60': 'sixty',
+  '70': 'seventy',
+  '80': 'eighty',
+  '90': 'ninety'
+}
+let hundreds = {
+  '100': 'one hundred',
+  '200': 'two hundred',
+  '300': 'three hundred',
+  '400': 'four hundred',
+  '500': 'five hundred',
+  '600': 'six hundred',
+  '700': 'seven hundred',
+  '800': 'eight hundred',
+  '900': 'nine hundred',
+}
+
+function
