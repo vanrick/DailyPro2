e6d7e85e81867df5b176f338ade6ed8aeb6876be
Rick Van <x.rckvan@gmail.com>
2017-07-20 14:14:31 -0600

    triangle

---
+++ CodeWars/5_mumbling/rv_solutions.js
@@ -0,0 +1,26 @@
+function accum(s) {
+	// your code
+}
+
+
+
+
+
+
+
+
+// function accum(s) {
+//   let output = '';
+//   let c = null;
+//   for (let i = 0; i < s.length; i++) {
+//     c = s[i].toLowerCase();
+//     for (let j = 0; j <= i; j++) {
+//       output += (j === 0) ? c.toUpperCase() : c;
+//     }
+//     if (i < s.length-1) {
+//       output += '-';
+//     }
+//   }
+//
+//   return output;
+// }
