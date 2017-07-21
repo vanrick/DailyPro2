d1fb75539c8e5f6f4913999e10cea6ecd584d4af
Rick Van <x.rckvan@gmail.com>
2017-07-21 16:23:45 -0600

    find divisor

--- CodeWars/5_mumbling/rv_solutions.js
+++ CodeWars/5_mumbling/rv_solutions.js
@@ -1,26 +1,25 @@
 function accum(s) {
-	// your code
+	let result = '';
+  let x;
+  for (var i = 0; i < s.length; i++) {
+    x = s[i].toLowerCase();
+    // console.log(x);
+    for (var j = 0; j <= i; j++) {
+      console.log('j ',j);
+      console.log('i ',i);
+      if (j === 0) {
+        result+=x.toUpperCase()
+      }else {
+        result+=x
+      }
+    }
+    if (i < s.length-1) {
+      console.log('bottom i ',i);
+      console.log('slength-1 ', s.length-1);
+      result += '-';
+    }
+  }
+  return result
 }
 
-
-
-
-
-
-
-
-// function accum(s) {
-//   let output = '';
-//   let c = null;
-//   for (let i = 0; i < s.length; i++) {
-//     c = s[i].toLowerCase();
-//     for (let j = 0; j <= i; j++) {
-//       output += (j === 0) ? c.toUpperCase() : c;
-//     }
-//     if (i < s.length-1) {
-//       output += '-';
-//     }
-//   }
-//
-//   return output;
-// }
+console.log(accum('RqaEzty'))
--- CodeWars/6_find-the-divisors/rv_solutions.js
+++ CodeWars/6_find-the-divisors/rv_solutions.js
@@ -1,3 +1,48 @@
 function divisors(integer) {
+  let result = []
+  let prime;
+    for (var i = 2; i < integer; i++) {
+      if (integer % i !== 0) {
+        prime = integer + ' is prime'
+      }else if (integer % i == 0) {
+        result.push(integer/i)
+      }
+    }
+  if (result.length > 0) {
+    return result.reverse()
+  }else {
+    return prime
+  }
+}
 
-};
+console.log(divisors(12));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function divisors(integer) {
+//     var divisors = [];
+//
+//     for (var i = integer - 1; i > 1; i--) {
+//         if (integer % i == 0) {
+//             divisors.push(i);
+//         }
+//     }
+//
+//     return divisors.length ? divisors.reverse() : (integer + ' is prime');
+// }
+//
+// console.log(divisors(12));
