eb75695f51185f8278fb423201a9275134fa7916
Rick Van <x.rckvan@gmail.com>
2017-07-28 19:43:23 -0600

    working on do while loops

---
+++ CodeWars/8_product-of-consecutive-Fib-numbers/rv_solution.js
@@ -0,0 +1,37 @@
+function fibonaccis(num){
+  let max = num,
+      index = 1,
+      
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var fibonacci_series = function (n)
+// {
+//   if (n===1){
+//     return [0, 1];
+//   }else{
+//     var s = fibonacci_series(n - 1);
+//     console.log(s);
+//     s.push(s[s.length - 1] + s[s.length - 2]);
+//     return s;
+//   }
+// };
+//
+// fibonacci_series(7)
--- practice/fibonacci.js
+++ practice/fibonacci.js
@@ -1,11 +1,8 @@
 var fibonacci_series = function (n)
 {
-  if (n===1)
-  {
+  if (n===1){
     return [0, 1];
-  }
-  else
-  {
+  }else{
     var s = fibonacci_series(n - 1);
     s.push(s[s.length - 1] + s[s.length - 2]);
     return s;
