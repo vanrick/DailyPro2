e1a1ab69e31bf7b6fc11d3c00cc326e1ce67453b
Rick Van <x.rckvan@gmail.com>
2017-08-10 19:46:08 -0600

    still working

--- CodeWars/13_give-me-a-diamond/index.js
+++ CodeWars/13_give-me-a-diamond/index.js
@@ -1,10 +1,13 @@
 function diamond(n){
-  console.log('new line'+'\n'+'other line');
-  
-  // return diam;
+  if (n < 0 || n%2==0) return null
+  for (var i = 0; i < n; i++) {
+    n[i]
+  }
+  // console.log('new line'+'\n'+'other line');
+
 
 }
-diamond(7)
+console.log(diamond(5));
 
 
 
@@ -59,3 +62,28 @@ diamond(7)
 // }
 // console.log( '\n' + diamond( 3 ) );
 // console.log( '\n' + diamond( 5 ) );
+
+
+// function diamond(n){
+//   if (n<3 || n%2==0)
+//     return null;
+//
+//   var str = '';
+//   var middle = (n-1)/2;//i of the middle row
+//
+//   for (var i=0; i<=middle-1; ++i)
+//   {
+//     var spaces = ' '.repeat(middle-i);
+//     str += spaces + '*'.repeat(i*2+1) + '\n';
+//   }
+//
+//   str += '*'.repeat(n) + '\n';
+//
+//   for (var i=middle+1; i<n; ++i)
+//   {
+//     var spaces = ' '.repeat(i-middle);
+//     str += spaces + '*'.repeat(n-(i-middle)*2) + '\n';
+//   }
+//
+//   return str;
+// }
