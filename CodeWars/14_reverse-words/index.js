afc724a8c6e96ed16b084bab671a39300fc537c7
Rick Van <x.rckvan@gmail.com>
2017-08-14 15:36:24 -0600

    rev

--- CodeWars/14_reverse-words/index.js
+++ CodeWars/14_reverse-words/index.js
@@ -1,10 +1,8 @@
 function reverseWords(str){
   let result = ''
-  let s = ''
   let splitStr = str.split(' ')
   for (var i = 0; i < splitStr.length; i++) {
     result += splitStr[i].split('').reverse().join('') + ' '
-    console.log(splitStr[i]);
   }
   console.log(result);
   return result
