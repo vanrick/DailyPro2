ab906bf60b83f6f553db8e20747a01ba0c5ea5d4
Rick Van <x.rckvan@gmail.com>
2017-08-29 19:13:42 -0600

    test

--- CodeWars/20_memoized-fibonacci/index.js
+++ CodeWars/20_memoized-fibonacci/index.js
@@ -15,7 +15,7 @@ console.log(memoized(8));
 console.log(memoized(7));
 console.log(memoized(8));
 console.log(memoized(9));
-console.log(memoized(5));
+console.log(memoized(4));
 console.log(tablez);
 function fib(num){if (num < 2) return num
 else return fib(num-2)+fib(num-1)}
