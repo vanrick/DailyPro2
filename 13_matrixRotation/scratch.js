cc1269ee613314dfcad64f1d6abe955f1bff81b7
Rick Van <x.rckvan@gmail.com>
2018-03-06 07:35:56 -0700

    matrix

---
+++ 13_matrixRotation/scratch.js
@@ -0,0 +1,22 @@
+var matrix = [
+    [1, 64, 23],
+    [22, 50, 2]
+];
+var matrix1 = [
+    [44, 23],
+    [21, 10],
+    [9, 28],
+    [85, 16],
+    [33, 18]
+];
+
+function rotateMatrix(matrix) {
+    let result = []
+    for (let i = 0; i < matrix[0].length; i++) {
+
+
+    }
+    return result
+}
+
+rotateMatrix(matrix)
