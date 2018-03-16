fd418fbdc2598575a30aa2ed37b85b8fcfa3ee85
Rick Van <x.rckvan@gmail.com>
2018-03-16 12:33:13 -0600

    st patties day

---
+++ 22_jsonFormatting/scratch.js
@@ -0,0 +1,47 @@
+let booksObj = {
+    "books": [{
+            "id": 1,
+            "name": "You don't know JS",
+            "author_id": 1,
+            "author_name": "Bonnie"
+        },
+        {
+            "id": 1,
+            "name": "You don't know JS",
+            "author_id": 2,
+            "author_name": "Eiseman"
+        },
+        {
+            "id": 2,
+            "name": "You know some JS",
+            "author_id": 1,
+            "author_name": "Bonnie"
+        }
+    ]
+}
+
+function authorArray(obj) {
+    let keys = Object.keys(obj)
+    let result = []
+    for (let i = 0; i < keys.length; i++) {
+        let bookz = obj[keys[i]]
+        for (let j = 0; j < bookz.length; j++) {
+            // console.log(bookz[j]);
+            if (keys[i].indexOf('1') > -1) {
+                result.push({
+                    'id': bookz[j].author_id,
+                    'name': bookz[j].author_name
+                })
+            }
+        }
+    }
+    console.log('result: ', result);
+    return result
+}
+
+function jsonFormatter(obj) {
+    let keys = Object.keys(obj)
+    let result = []
+}
+authorArray(booksObj)
+jsonFormatter(booksObj)
