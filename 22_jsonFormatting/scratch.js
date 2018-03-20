8bf92d703dfd135febc79c59645e5ed527106d28
Rick Van <x.rckvan@gmail.com>
2018-03-20 08:44:53 -0600

    working on website

--- 22_jsonFormatting/scratch.js
+++ 22_jsonFormatting/scratch.js
@@ -20,28 +20,6 @@ let booksObj = {
     ]
 }
 
-function authorArray(obj) {
-    let keys = Object.keys(obj)
-    let result = []
-    for (let i = 0; i < keys.length; i++) {
-        let bookz = obj[keys[i]]
-        for (let j = 0; j < bookz.length; j++) {
-            // console.log(bookz[j]);
-            if (keys[i].indexOf('1') > -1) {
-                result.push({
-                    'id': bookz[j].author_id,
-                    'name': bookz[j].author_name
-                })
-            }
-        }
-    }
-    console.log('result: ', result);
-    return result
-}
+function jsonConverter() {
 
-function jsonFormatter(obj) {
-    let keys = Object.keys(obj)
-    let result = [].
 }
-authorArray(booksObj)
-jsonFormatter(booksObj)
