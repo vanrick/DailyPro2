ad2683a66db97b6d80aa944eebcea8074aefff25
Rick Van <x.rckvan@gmail.com>
2018-03-21 07:49:47 -0600

    testing

--- 22_jsonFormatting/scratch.js
+++ 22_jsonFormatting/scratch.js
@@ -20,6 +20,22 @@ let booksObj = {
     ]
 }
 
-function jsonConverter() {
+function jsonConverter(obj) {
+    let keys = Object.keys(obj)
+        // console.log(keys);
+    let result = {}
+    for (let i = 0; i < keys.length; i++) {
+        // console.log(obj[keys]);
+        let inside = obj[keys]
+        for (let j = 0; j < inside.length; j++) {
+            // console.log(inside[j]['id']);
+            if (inside[j]['id'] == 1) {
+                console.log(inside[j]);
 
+            }
+
+        }
+    }
 }
+
+jsonConverter(booksObj)
