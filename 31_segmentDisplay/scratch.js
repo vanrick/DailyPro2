3d15dee715cef36e69d2f97fb5968595ad3f684a
Rick Van <x.rckvan@gmail.com>
2018-04-09 11:18:45 -0600

    nums

--- 31_segmentDisplay/scratch.js
+++ 31_segmentDisplay/scratch.js
@@ -1,3 +1,56 @@
+var numbers = {
+    '1': {
+        line1: '   ',
+        line2: '  |',
+        line3: '  |'
+    },
+    '2': {
+        line1: ' __',
+        line2: ' _|',
+        line3: '|__'
+    },
+    '3': {
+        line1: '__ ',
+        line2: '__|',
+        line3: '__|'
+    },
+    '4': {
+        line1: '   ',
+        line2: '|_|',
+        line3: '  |'
+    },
+    '5': {
+        line1: ' _ ',
+        line2: '|_ ',
+        line3: ' _|'
+    },
+    '6': {
+        line1: ' _ ',
+        line2: '|_ ',
+        line3: '|_|'
+    },
+    '7': {
+        line1: '__ ',
+        line2: '  |',
+        line3: '  |'
+    },
+    '8': {
+        line1: ' _ ',
+        line2: '|_|',
+        line3: '|_|'
+    },
+    '9': {
+        line1: ' _ ',
+        line2: '|_|',
+        line3: ' _|'
+    },
+    '0': {
+        line1: ' _ ',
+        line2: '| |',
+        line3: '|_|'
+    }
+}
+
 function sevenSegment(num) {
 
 }
