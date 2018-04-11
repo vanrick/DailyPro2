0d0b582b6f9eff307c66ba84d8877bf42ec4005d
Rick Van <x.rckvan@gmail.com>
2018-04-11 10:54:35 -0600

    practice OOP

--- 31_segmentDisplay/scratch.js
+++ 31_segmentDisplay/scratch.js
@@ -1,84 +1,78 @@
 var numbers = {
-        '1': {
-            line1: '   ',
-            line2: '  |',
-            line3: '  |'
-        },
-        '2': {
-            line1: ' __',
-            line2: ' _|',
-            line3: '|__'
-        },
-        '3': {
-            line1: '__ ',
-            line2: '__|',
-            line3: '__|'
-        },
-        '4': {
-            line1: '   ',
-            line2: '|_|',
-            line3: '  |'
-        },
-        '5': {
-            line1: ' _ ',
-            line2: '|_ ',
-            line3: ' _|'
-        },
-        '6': {
-            line1: ' _ ',
-            line2: '|_ ',
-            line3: '|_|'
-        },
-        '7': {
-            line1: '__ ',
-            line2: '  |',
-            line3: '  |'
-        },
-        '8': {
-            line1: ' _ ',
-            line2: '|_|',
-            line3: '|_|'
-        },
-        '9': {
-            line1: ' _ ',
-            line2: '|_|',
-            line3: ' _|'
-        },
-        '0': {
-            line1: ' _ ',
-            line2: '| |',
-            line3: '|_|'
-        }
+    '1': {
+        line1: '   ',
+        line2: '  |',
+        line3: '  |'
+    },
+    '2': {
+        line1: ' __',
+        line2: ' _|',
+        line3: '|__'
+    },
+    '3': {
+        line1: '__ ',
+        line2: '__|',
+        line3: '__|'
+    },
+    '4': {
+        line1: '   ',
+        line2: '|_|',
+        line3: '  |'
+    },
+    '5': {
+        line1: ' _ ',
+        line2: '|_ ',
+        line3: ' _|'
+    },
+    '6': {
+        line1: ' _ ',
+        line2: '|_ ',
+        line3: '|_|'
+    },
+    '7': {
+        line1: '__ ',
+        line2: '  |',
+        line3: '  |'
+    },
+    '8': {
+        line1: ' _ ',
+        line2: '|_|',
+        line3: '|_|'
+    },
+    '9': {
+        line1: ' _ ',
+        line2: '|_|',
+        line3: ' _|'
+    },
+    '0': {
+        line1: ' _ ',
+        line2: '| |',
+        line3: '|_|'
     }
-    // console.log(numbers[0]);
+}
 
 function sevenSegment(num) {
-    let stringNum = String(num).split('')
-    let linez = {
+    num = String(num).split('');
+    var result = {
         line1: '',
         line2: '',
         line3: ''
     }
-    let keys = Object.keys(numbers)
-        // console.log(stringNum);
-    for (let i = 0; i < stringNum.length; i++) {
-        const inside = stringNum[i];
-        // console.log(inside);
-        linez.line1 += numbers[stringNum[i]].line1 + ' ';
-        linez.line2 += numbers[stringNum[i]].line2 + ' ';
-        linez.line3 += numbers[stringNum[i]].line3 + ' ';
+    for (let i = 0; i < num.length; i++) {
+        const element = num[i];
+        // console.log(element);
+        result.line1 += numbers[element].line1 + ' '
+        result.line2 += numbers[element].line2 + ' '
+        result.line3 += numbers[element].line3 + ' '
     }
-    let logNum = ''
-    for (const key in linez) {
-        if (linez.hasOwnProperty(key)) {
-            const element = linez[key];
-            console.log(element);
-
-        }
+    // console.log(result);
+    let logResult = ''
+    for (var key in result) {
+        logResult += result[key] + '\n'
     }
-    console.log(logNum);
-
-    return logNum
+    console.log(logResult);
+    return logResult
 }
 
-sevenSegment(1000)
+
+sevenSegment(100)
