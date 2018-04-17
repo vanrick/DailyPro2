fd85b62ef73d1abf73743fe347b5b12922a7330d
Rick Van <x.rckvan@gmail.com>
2018-04-17 08:13:59 -0600

    diag

--- 33_DiagonalSum/scratch.js
+++ 33_DiagonalSum/scratch.js
@@ -1,3 +1,25 @@
-function diagonals() {
+let threeBythree = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
 
+function diagonals(array) {
+    let result = 0
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i][i]
+
+        console.log(element);
+        console.log(array[i][array.length - 1 - i]);
+        result += element + array[i][array.length - 1 - i]
+            // for (let j = array.length - 1; j >= 0; j--) {
+            //     const element2 = array[j][j];
+            //     console.log(element2);
+
+        // }
+
+    }
+    console.log(result);
+    return result
 }
+diagonals(threeBythree)
