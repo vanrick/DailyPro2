63052604e2954abba67e37c90864e1247b398d05
Rick Van <x.rckvan@gmail.com>
2018-04-19 09:18:43 -0600

    arrays

--- 34_arrays/scratch.js
+++ 34_arrays/scratch.js
@@ -3,27 +3,39 @@ function array() {
     newArray.length = 0
     newArray.push = function(val) {
         newArray[newArray.length] = val;
-        newArray.length += 1
+        newArray.length++
     }
-
     newArray.pop = function() {
         if (newArray.length > 0) {
-            newArray.length -= 1
-            var popArr = newArray[newArray.length];
+            newArray.length--
+                let arr = newArray[newArray.length]
             delete newArray[newArray.length]
         }
-        return popArr
+        return arr
     }
-    console.log(newArray);
-
     return newArray
 }
-let arr = array();
-arr.push(100);
-console.log(arr.length);
-arr.push(10);
-console.log(arr.length);
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr.pop());
-console.log(arr.length);
+array()
+let arr = array()
+console.log(arr.push);
+arr.push('stuff')
+console.log(arr);
+arr.push('other stuff')
+console.log(arr);
+arr.pop()
+console.log(arr);
+console.log('x ', arr.pop());
+
+
+
+
+
+// let arr = array();
+// arr.push(100);
+// console.log(arr.length);
+// arr.push(10);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr.pop());
+// console.log(arr.length);
