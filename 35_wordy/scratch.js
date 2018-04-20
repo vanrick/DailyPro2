8da340ce38388a4636662d9db660666c19dee4ae
Rick Van <x.rckvan@gmail.com>
2018-04-20 11:33:23 -0600

    setting up wordy

---
+++ 35_wordy/scratch.js
@@ -0,0 +1,36 @@
+let word_problem = "What is 5 plus 13?";
+
+let word_problem2 = 'What is 7 minus 5?'
+
+let word_problem3 = 'What is 6 multiplied by 4?'
+
+let word_problem4 = 'What is 25 divided by 5?'
+
+let word_problem5 = 'What is 5 plus 13 plus 6?'
+
+let word_problem6 = 'What is 7 minus 5 minus 1?'
+
+let word_problem7 = 'What is 9 minus 3 plus 5?'
+
+let word_problem8 = 'What is 3 plus 5 minus 8?'
+
+let word_problem9 = 'What is 5 plus 13?'
+
+let word_problem10 = 'What is 7 minus 5?'
+
+let word_problem11 = 'What is 6 times 4?'
+
+let word_problem12 = 'What is 25 divided by 5?'
+
+let word_problem13 = 'What is 78 plus 5 minus 3?'
+
+let word_problem14 = 'What is 18 times 3 plus 16?'
+
+let word_problem15 = 'What is 4 times 3 divided by 6?'
+
+let word_problem16 = 'What is 4 plus 3 times 2?'
+
+function wordy(problem) {
+    let result = 0;
+
+}
