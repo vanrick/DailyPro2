d5edf7873bbca9b1f73c510d598a2847dff7c57e
Rick Van <x.rckvan@gmail.com>
2018-05-07 12:06:47 -0600

    working on squares

--- CodeWars/1_findNextSquare/scratch.js
+++ CodeWars/1_findNextSquare/scratch.js
@@ -1,8 +1,9 @@
 function findNextSquare(num) {
     num = Math.sqrt(num);
     if (Number.isInteger(num)) {
-        return Math.pow(sq + 1, 2)
+        return Math.pow(num + 1, 2)
     } else {
         return -1
     }
 }
+console.log(findNextSquare(121));
