ebf8491972fb2b3e54f02139f15162e125fcc9da
Rick Van <x.rckvan@gmail.com>
2018-05-31 14:08:26 -0600

    call() and appl() methods

---
+++ JS interview Prep: Practice Problems/14. call and apply methods/index.js
@@ -0,0 +1,7 @@
+const car1 = {
+    brand: "Porsche",
+    getCarDescription: function(cost, year, color) {
+        console.log(`This care is a ${}. The price is $${cost}. The year is ${year}. The color is ${color}.\n`);
+
+    }
+};
---
+++ JS interview Prep: Practice Problems/14. call and apply methods/readme.md
@@ -0,0 +1,6 @@
+Describe the JS call() and apply() methods.
+
+address the following:
+1. how do they function?
+2. what arguments do they take?
+3. how are they different?
